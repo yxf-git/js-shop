cb64bd3e0d4d2acd62f36f8b59bf5d96f51c625e
[qq491900274] <491900274@qq.com>
2018-10-15 14:50:24 +0800

    添加登陆页面与样式

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,19 +1,20 @@
 <?php
-namespace app\index\controller;
+namespace app\admin\controller;
 use think\Controller;
 use think\View;
 use think\Request;
 use think\Session;
-use app\index\lib\Home;
+use app\model\lib\Home;
 
 
 class Index extends Controller
 {	
 	public function __construct(){
 		parent::__construct();
-	    //判断用户是否登录
+		
+		//判断用户是否登录
         if(empty($_SESSION['user'])){
-        	$this->error('请登录！',url('Login/index'));
+        	$this->error('请登录！',"HTTP://{$_SERVER['SERVER_NAME']}/js-shop?s=admin/Login/index");
         }
 	}
 		
--- application/config.php
+++ application/config.php
@@ -141,9 +141,8 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-    	'sourceindex' =>'/static/index',
-    	'sourceadmin' =>'/static/admin',
-    	'sourcepublic' =>'/static/public',
+    	'__PUBLIC__' =>'/js-shop/public/static',
+       
     	],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
