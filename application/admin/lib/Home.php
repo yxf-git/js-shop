27931660a6bb2a2d1411e27ba5a7d7db51ad2256
[qq491900274] <491900274@qq.com>
2018-10-15 14:54:01 +0800

    添加登陆

---
+++ application/admin/lib/Home.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\model\lib;
+use think\Controller;
+use think\View;
+use think\Request;
+use think\Session;
+
+
+class Home extends Controller
+{	
+    public function __construct(){
+    	parent::__construct();
+    }
+   
+    public function hell(){
+    	$name = 'helloword1';
+    	$this->assign('name',$name);
+    }
+}
