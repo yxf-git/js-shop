cdec7d0969d2164618b3ad23af2ca68347887195
[qq491900274] <491900274@qq.com>
2018-10-16 15:03:10 +0800

    密码验证方法

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -18,10 +18,22 @@ class Login extends Controller
     	$this->assign('name','123');
     	return $this->fetch('login'); 
     }
+    //登陆
     public function Login_in()
     {
+      var_dump(BASE_URL);
       $this->LoginModel =  new \app\index\model\LoginModvel();
       $result = $this->LoginModel->hello('helloword');
       return json_encode($result);
     }
+
+    //验证密码功能
+    public function getpwd(){
+      if ($_POST['user']=='1' && $_POST['pwd']=='2') {
+        return json_encode('msg'=>'登陆成功！','state'=>'1');  
+      }else{
+        return json_encode('msg'=>'登陆失败！','state'=>'2');
+      }
+    }
+
 }
